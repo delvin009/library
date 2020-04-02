32a590492601afc66a37da4b882815b5d596db89
delvin009 <gracynorbert3@gmail.com>
2020-04-02 19:28:45 +0530

    BookReservationTest

---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Book;
+
+class BookReservationTest extends TestCase
+{
+    //test
+    public function a_book_can_be_added_to_the_library()
+    {
+        $response = $this->post('/books', [
+            'title' => 'cool book title',
+            'author' => 'Bichu'
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+    }    
+}

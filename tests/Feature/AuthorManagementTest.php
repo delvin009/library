d8bc6f3b86fe5683f9730e2a5f3c711c9540acb0
delvin009 <58433164+delvin009@users.noreply.github.com>
2020-04-04 19:11:13 +0530

    adds author resource

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Author;
+use Carbon\Carbon;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+   /** @test */
+   public function an_author_can_be_created()
+   {
+       $this->withoutExceptionHandling();
+
+        $this->post('/author', [
+            'name' => 'demo name',
+            'dob' => '12/25/1992',
+        ]);
+
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+
+        $this->assertEquals('1992/25/12', $author->first()->dob->format('Y/d/m'));
+   }
+}
